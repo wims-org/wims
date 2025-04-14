4fb0085154d037ae24d0a757c778b8b0d15b92ee
EeveesEyes <a@kailus.dev>
2025-04-14 17:39:21 +0200

    feat(images): beauty

--- vue_frontend/src/components/fields/ImageThumbnailField.vue
+++ vue_frontend/src/components/fields/ImageThumbnailField.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="image-thumbnail-field">
-    <div v-if="value && value.length" class="d-flex flex-wrap">
+  <div class="image-thumbnail-field p-2">
+    <label :for="name">{{ label }}</label>
+    <div class="thumbnail-container-wrapper d-flex flex-wrap align-items-center">
       <div
         v-for="(image, index) in value"
         :key="index"
@@ -8,40 +9,29 @@
         @click="openImageModal(image)"
       >
         <img :src="image" class="thumbnail" alt="Image Thumbnail" />
-        <button
-          class="btn btn-danger btn-sm mt-1"
-          @click.stop="removeImage(index)"
-        >
-          Remove
+        <button class="remove-btn" @click.stop="removeImage(index)">
+          <font-awesome-icon icon="times" />
+        </button>
+      </div>
+      <div v-if="value.length === 0" class="text-center m-2">
+        <font-awesome-icon icon="camera" size="xl" />
+        <p>No images</p>
+      </div>
+      <div class="add-image-container m-2">
+        <input
+          ref="cameraInput"
+          type="file"
+          class="d-none"
+          accept="image/*"
+          capture="environment"
+          @change="addImage"
+        />
+        <button type="button" class="btn btn-primary add-image-btn" @click="triggerCameraInput">
+          +
         </button>
       </div>
     </div>
-    <p v-else>No images available.</p>
-    <div class="add-image-container mt-3">
-      <input
-        ref="cameraInput"
-        type="file"
-        class="d-none"
-        accept="image/*"
-        capture="environment"
-        @change="addImage"
-      />
-      
-    </div>
-    <button
-        type="button"
-        class="btn btn-primary add-image-btn"
-        @click="triggerCameraInput"
-      >
-        +
-      </button>
-    <!-- Use the new ImageModal component -->
-    <ImageModal
-      v-if="showModal && selectedImage"
-      :image="selectedImage"
-      @close="closeImageModal"
-    />
-    
+    <ImageModal v-if="showModal && selectedImage" :image="selectedImage" @close="closeImageModal" />
   </div>
 </template>
 
@@ -55,6 +45,14 @@ export default defineComponent({
     ImageModal,
   },
   props: {
+    name: {
+      type: String,
+      required: false,
+    },
+    label: {
+      type: String,
+      required: false,
+    },
     value: {
       type: Array as PropType<string[]>,
       required: true,
@@ -129,6 +127,27 @@ export default defineComponent({
   object-fit: cover;
   border: 1px solid #ddd;
   border-radius: 4px;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+}
+
+.remove-btn {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  background: none;
+  border: none;
+  cursor: pointer;
+  color: #ff0000;
+  border-radius: 50%;
+
+  &:hover {
+    color: #ff0000;
+    transform: scale(1.2);
+    background: rgba(0, 0, 0, 0.2);
+  }
 }
 
 .add-image-container {
