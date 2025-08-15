0313ca642997968ae0bf6a9b3b62e58967964570
EeveesEyes <a@kailus.dev>
2025-08-16 00:13:24 +0200

    feat(healthz): add backend ready route

--- backend/src/routers/healthz.py
+++ backend/src/routers/healthz.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.responses import PlainTextResponse
 
 router = APIRouter(prefix="/healthz", tags=["healthz"])
@@ -7,4 +7,12 @@ router = APIRouter(prefix="/healthz", tags=["healthz"])
 @router.get("", response_class=PlainTextResponse)
 async def healthz():
     # TODO: Actually check things like DB and MQTT connectivity and then return appropiate status code
+    return "OK"
+
+
+@router.get("/ready", response_class=PlainTextResponse)
+async def ready(request: Request):
+    backend_service = request.app.state.backend_service
+    if not backend_service.is_ready():
+        return PlainTextResponse("Service not ready", status_code=503)
     return "OK"
