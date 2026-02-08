b90e4902a4c2ace70bc6b94e484a92e8a3a2b5ab
fleaz <mail@felixbreidenstein.de>
2026-02-08 18:08:41 +0100

    cli: Added scanner function

--- cli/cli.py
+++ cli/cli.py
@@ -1,9 +1,10 @@
 import typer
-from src import items, users
+from src import items, users,scanner
 
 app = typer.Typer(add_completion=False, no_args_is_help=True)
 app.add_typer(users.app, name="users")
 app.add_typer(items.app, name="items")
+app.add_typer(scanner.app, name="scanner")
 
 if __name__ == "__main__":
     app()
