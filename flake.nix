e7c9eac6c78fcf78cc825f509b92c1cc0c243ab1
fleaz <mail@felixbreidenstein.de>
2026-05-16 18:03:08 +0200

    hardware: Build scanner with esp-idf, fix boot sequence.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     git-hooks = {
       url = "github:cachix/git-hooks.nix";
@@ -19,6 +20,7 @@
         system:
         let
           pkgs = inputs.nixpkgs.legacyPackages.${system};
+          unstable-pkgs = inputs.unstable.legacyPackages.${system};
           inherit (pkgs) lib;
         in
         {
@@ -59,14 +61,15 @@
 
               # backend
               uv
-              python314
+              python313
               mariadb.client
 
               # hardware
               python3Packages.pyserial
               esptool
-              esphome
+              unstable-pkgs.esphome
               picocom
+              platformio
             ];
 
           };
