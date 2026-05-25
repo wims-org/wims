7ed2f04281e51b90589da45bccedd541950f4533
fleaz <mail@felixbreidenstein.de>
2026-05-25 13:57:35 +0200

    add validators to the settings object

--- backend/src/routers/files.py
+++ backend/src/routers/files.py
@@ -1,3 +1,4 @@
+import os
 from hashlib import md5
 from pathlib import Path
 from typing import Annotated
@@ -23,8 +24,8 @@ async def create_file(
 ):
     data = file.file.read()
     hash_name = md5(data).hexdigest() + "." + file.filename.split(".")[-1]
-    asset_uri = "/" + settings.asset_uri_prefix.strip("/") + "/" + hash_name
-    fs_path: Path = settings.data_path / hash_name
+    asset_uri = os.path.join(settings.asset_uri_prefix,hash_name)
+    fs_path: Path = Path(settings.data_path) / Path(hash_name)
     db_file = (await session.execute(select(File).where(File.uri == asset_uri))).scalar_one_or_none()
     if db_file:
         if not fs_path.exists():
